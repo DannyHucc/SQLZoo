18c0ddd99cd431925b217f2b3a3531a849ee38eb
DannyHucc <dannyhu10011101@gmail.com>
2024-03-19 10:34:16 +0800

    feat: add 04.SELECT_within_SELECT_Tutorial 5

--- 04.SELECT_within_SELECT_Tutorial/SELECT-within-SELECT-Tutorial.sql
+++ 04.SELECT_within_SELECT_Tutorial/SELECT-within-SELECT-Tutorial.sql
@@ -38,3 +38,14 @@ WHERE population >
      (SELECT population
       FROM world
       WHERE name IN ('Germany'));
+
+-- 5. Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+SELECT name,
+       CONCAT(ROUND(
+          population / 
+          (SELECT population
+           FROM world 
+           WHERE name = 'Germany')
+          * 100), '%')
+FROM world
+WHERE continent = 'Europe';
