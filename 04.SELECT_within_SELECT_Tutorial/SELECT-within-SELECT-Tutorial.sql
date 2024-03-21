d651a8bf0b59760713b40e00d81449c61869a088
DannyHucc <dannyhu10011101@gmail.com>
2024-03-21 11:16:36 +0800

    feat: add 04.SELECT_within_SELECT_Tutorial 6-10

--- 04.SELECT_within_SELECT_Tutorial/SELECT-within-SELECT-Tutorial.sql
+++ 04.SELECT_within_SELECT_Tutorial/SELECT-within-SELECT-Tutorial.sql
@@ -49,3 +49,49 @@ SELECT name,
           * 100), '%')
 FROM world
 WHERE continent = 'Europe';
+
+-- 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+SELECT name
+FROM world
+WHERE gdp > ALL
+     (SELECT gdp
+      FROM world
+      WHERE gdp IS NOT NULL AND
+            continent = 'Europe');
+
+-- 7. Find the largest country (by area) in each continent, show the continent, the name and the area:
+-- The above example is known as a correlated or synchronized sub-query.
+SELECT continent, name, area
+FROM world x
+WHERE area >= ALL
+     (SELECT area
+      FROM world y
+      WHERE area IS NOT NULL AND
+            x.continent = y.continent);
+
+-- 8. List each continent and the name of the country that comes first alphabetically.
+SELECT continent, name
+FROM world x
+WHERE name <= ALL
+     (SELECT name
+      FROM world y
+      WHERE x.continent = y.continent);
+
+-- 9. Find the continents where all countries have a population <= 25000000.
+--    Then find the names of the countries associated with these continents.
+--    Show name, continent and population.
+SELECT name, continent, population
+FROM world x
+WHERE 25000000 >= ALL
+     (SELECT population
+      FROM world y
+      WHERE x.continent = y.continent);
+
+-- 10. Some countries have populations more than three times that of all of their neighbours (in the same continent). Give the countries and continents.
+SELECT name, continent
+FROM world x
+WHERE population > ALL
+     (SELECT 3 * population
+      FROM world y
+      WHERE x.continent = y.continent AND 
+            x.name <> y.name);
